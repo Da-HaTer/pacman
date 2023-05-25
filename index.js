c94adc513e49fe79f3a616b52a83a87ae701fe2f
Da-HaTer <oussamah4boubi@gmail.com>
2023-05-25 18:51:44 +0100

    simple collision, manual movement

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const c=canvas.getContext('2d')
 canvas.width=innerWidth
 canvas.height=innerHeight
 var show_grid=false
-const fps=60
+const fps=30
+
+function beep(){
+    var audio = new Audio('sound.wav');
+    audio.play();
+}
 
 const keys={
     z:{pressed: false},
     q:{pressed: false},
     d:{pressed: false},
-    s:{pressed: false}
+    s:{pressed: false},
 }
+let laskey=''
 //ghost house coordinates: x: 11 - 16 y: 14 - 16
 class Player{
     constructor({position,speed,radius}){
@@ -19,11 +25,11 @@ class Player{
         this.velocity={x:0,y:0}
         this.radius=radius
         this.color="yellow"
-        console.log(this.speed)
+        console.log(this.radius)
     }
     draw(){
         c.beginPath()
-        let pacman_pos={x:w/2+tz*coords.x+tz/2,y:tz*coords.y+tz*4+tz/2}
+        let pacman_pos={x:w/2+tz*this.position.x+tz/2,y:tz*this.position.y+tz*4+tz/2}
         c.arc(pacman_pos.x,pacman_pos.y,this.radius,0,Math.PI*2)
         c.fillStyle=this.color
         c.fill()
@@ -32,50 +38,84 @@ class Player{
     update(map){
         var grid=map.routes
         this.draw()
+        var can_turn_y=true
+        var can_turn_x=true
         var x=this.position.x
         var y=this.position.y+4
-
-        this.position.x+=this.velocity.x
-        this.position.y+=this.velocity.y
-
-        if (grid[y][x]==-1){//i'm stuck inside a wall ///maybe just reset my position
+        let xc=~~x
+        let yc=~~y
+        let r=0.5
+        let v=this.velocity
+        //collision detection ?
+ 
+        if (grid[yc][(~~(x+v.x))]<0){ //left
+            can_turn_x=false
+            // this.velocity.x=0
+            // keys.q.pressed=false
+            this.color="orange"
+        }
+        if (grid[yc][~~(x+r+v.x)]<0){ //left
+            // this.velocity.x=0
+            can_turn_x=false
+            // keys.q.pressed=false
+            this.color="orange"
+        }
+        if (grid[~~(y+v.y)][xc]<0){//up
+            this.color="orange"
+            can_turn_y=false
+            // this.velocity.y= 0
+        }
+        if (grid[~~(y+r+v.y)][xc]<0){//up
+            this.color="orange"
+            // this.velocity.y= 0
+            can_turn_y=false
+        }
+        if (can_turn_x){this.position.x+=this.velocity.x
+        }
+        if (can_turn_y){this.position.y+=this.velocity.y}
+        if ((~~this.position.x)!=xc){
+            this.velocity.y=0
+        }
+        if ((~~this.position.y+4)!=yc){
+            this.velocity.x=0
+        }
+        if (grid[yc][xc]==-1){//i'm stuck inside a wall ///maybe just reset my position
             this.color="red"
-            setTimeout(null, 3000);
-            this.position={x:13,y:22}
             this.velocity={x:0,y:0}
-            this.color="yellow"
             //reset pos
         }
         else{   
-            grid[y][x]=0 //i'm not inside a wall
-            if (keys.q.pressed ){ //left && grid[y][x-1]>=0
+            this.color="yellow"
+            grid[yc][xc]=0 //i'm not inside a wall
+            if (keys.q.pressed && laskey=='q'){ //left && grid[y][x-1]>=0
                 this.velocity.x= -this.speed
-                // keys.q.pressed=false
+                // this.velocity.y=0
+                keys.q.pressed=false
             }
-            if (keys.z.pressed && grid[y-1][x]>=0){//up
+            else if (keys.z.pressed && laskey=='z'){//up && grid[y-1][x]>=0
                 this.velocity.y= -this.speed
+                // this.velocity.x=0
                 keys.z.pressed=false
             }
-            if (keys.d.pressed && grid[y][x+1]>=0){//right
+            else if (keys.d.pressed && laskey=='d'){//right && grid[y][x+1]>=0
                 // grid[y][x-1]=0
                 this.velocity.x= this.speed
+                // this.velocity.y=0
                 keys.d.pressed=false
             }
-            if (keys.s.pressed && grid[y+1][x]>=0){//down
+            else if (keys.s.pressed && laskey=='s'){//down && grid[y+1][x]>=0
                 // grid[y][x-1]=0
                 this.velocity.y= this.speed
+                // this.velocity.x=0
                 keys.s.pressed=false
             }
-            if (keys.q.pressed && x==0){//tunnels
+            if (x<0){//tunnels
                 this.position.x+=27
-                keys.z.pressed=false
             }
-            if (keys.d.pressed && x==27){//tunnels
+            if (x>27){//tunnels
                 this.position.x-=27
-                keys.z.pressed=false
             }
-            // this.position.x+=this.veloocity.x
-            // this.position.y+=this.veloocity.y
+
         }
     }
 }
@@ -104,7 +144,7 @@ var tz=map.tileSize
 var coords={x:13,y:22} //init positon 11 , 22
 // //offset: x=x+1 y=y+4
 const player=new Player({
-    position:coords,speed:1,radius: map.tileSize/2
+    position:coords,speed:3,radius: map.tileSize/2
 })
 
 function animate(){
@@ -132,18 +172,22 @@ window.addEventListener('keydown', ({key})=>{
         case 'z':
             keys.z.pressed=true
             keys.s.pressed=false
+            laskey="z"
             break
         case 'q':
             keys.q.pressed=true
             keys.d.pressed=false
+            laskey='q'
             break
         case 's':
             keys.s.pressed=true
             keys.z.pressed=false
+            laskey='s'
             break
         case 'd':
             keys.d.pressed=true
             keys.q.pressed=false
+            laskey='d'
             break
         case 'x':
             map.wallFillColor = randomColor();
